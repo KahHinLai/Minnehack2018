a6eb66e1b7e18a8f667a74cbd66f88639592f117
danyang <wang6132@umn.edu>
2018-01-28 01:43:48 -0600

    .

---
+++ PersonalSportPlan/app/src/main/java/com/example/personalsportplan/fillingFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.personalsportplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fillingFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filling_form);
+    }
+}
--- PersonalSportPlan/app/src/main/java/com/example/personalsportplan/startFormActivity.java
+++ PersonalSportPlan/app/src/main/java/com/example/personalsportplan/startFormActivity.java
@@ -1,7 +1,9 @@
 package com.example.personalsportplan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class startFormActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class startFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_form);
+    }
+
+    public void fillingForm(View view) {
+        // do something in continue button
+        Intent intent = new Intent(this, fillingFormActivity.class);
+        startActivity(intent);
     }
 }
