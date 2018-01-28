1bed4f7a6637d8cc2f6175fde92d4baa35aac221
Kah Hin Lai <laixx330@umn.edu>
2018-01-28 05:26:44 -0600

    DB IO

--- PersonalSportPlan/app/src/main/java/com/example/personalsportplan/DashBoardActivity.java
+++ PersonalSportPlan/app/src/main/java/com/example/personalsportplan/DashBoardActivity.java
@@ -2,12 +2,44 @@ package com.example.personalsportplan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+    TextView max_speed;
+    TextView max_acceleration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        max_speed = findViewById(R.id.max_speed);
+        max_acceleration = findViewById(R.id.max_acceleration);
+        try {
+            FileInputStream fileIn = openFileInput("mytextfile.txt");
+            InputStreamReader InputRead = new InputStreamReader(fileIn);
+            char[] inputBuffer= new char[100];
+            String s="";
+            int charRead;
+
+            while ((charRead=InputRead.read(inputBuffer))>0) {
+                // char to string conversion
+                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                s +=readstring;
+            }
+            InputRead.close();
+            String items[] = s.split(" ");
+            max_speed.setText(items[0]);
+            max_acceleration.setText(items[1]);
+        } catch (Exception e) {
+            String s = "N/A";
+            max_speed.setText((CharSequence)s);
+        }
+
     }
 }
--- PersonalSportPlan/app/src/main/java/com/example/personalsportplan/fillingFormActivity.java
+++ PersonalSportPlan/app/src/main/java/com/example/personalsportplan/fillingFormActivity.java
@@ -1,13 +1,47 @@
 package com.example.personalsportplan;
 
+
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class fillingFormActivity extends AppCompatActivity {
 
+    EditText max_speed;
+    EditText max_acceleration;
+    EditText max_flight_climbed;
+    EditText max_heart_rate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filling_form);
+
+        max_speed = (EditText)findViewById(R.id.max_speed);
+        max_acceleration = (EditText) findViewById(R.id.max_acceleration);
+        max_flight_climbed = (EditText) findViewById(R.id.max_flights_climbed);
+        max_heart_rate = (EditText) findViewById(R.id.max_heart_rate);
     }
+
+    public void WriteBtn(View v) {
+        // add-write text into file
+        try {
+            FileOutputStream fileout = openFileOutput("mytextfile.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
+            String inputs = "";
+            inputs += max_speed.getText().toString() + " " + max_acceleration.getText().toString()
+                    + " " + max_flight_climbed.getText().toString() + " "
+                    + max_heart_rate.getText().toString();
+            outputWriter.write(inputs);
+            outputWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
